23a722f844a6df74b5f21976c012e62d4aeee04b
evaanthony <evayuba11@gmail.com>
2023-02-02 05:43:04 -0900

    first commit

---
+++ trees/src/routes/viewTree/+page.svelte
@@ -0,0 +1,86 @@
+<svelte:head>
+	<title>View Trees</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>View Trees</h1>
+</div>
+<div class="text-column">
+	<table role="grid" aria-describedby="DataTables_Table_0_info">
+		<thead>
+			<tr>
+				<th tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1">S/N</th>
+				<th tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1">Tree Name</th>
+				<th tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1">Tree Type</th>
+				<th tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1">Tree Location</th>
+				<th tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1">Tree Size</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<th>1</th>
+				<th>Mango</th>
+				<th>Fruit</th>
+				<th>Mwera</th>
+				<th>12</th>
+			</tr>
+			<tr>
+				<th>2</th>
+				<th>Banana</th>
+				<th>Fruit</th>
+				<th>Fuoni</th>
+				<th>8</th>
+			</tr>
+			<tr>
+				<th>3</th>
+				<th>Mango</th>
+				<th>Fruit</th>
+				<th>Mwera</th>
+				<th>15</th>
+			</tr>
+			<tr>
+				<th>4</th>
+				<th>Banana</th>
+				<th>Fruit</th>
+				<th>Fuoni</th>
+				<th>13</th>
+			</tr>
+			<tr>
+				<th>5</th>
+				<th>Mango</th>
+				<th>Fruit</th>
+				<th>Mwera</th>
+				<th>16</th>
+			</tr>
+			<tr>
+				<th>6</th>
+				<th>Banana</th>
+				<th>Fruit</th>
+				<th>Fuoni</th>
+				<th>11</th>
+			</tr>
+			<tr>
+				<th>7</th>
+				<th>Mango</th>
+				<th>Fruit</th>
+				<th>Mwera</th>
+				<th>15</th>
+			</tr>
+			<tr>
+				<th>8</th>
+				<th>Banana</th>
+				<th>Fruit</th>
+				<th>Fuoni</th>
+				<th>18</th>
+			</tr>
+			<tr>
+				<th>9</th>
+				<th>Mpera</th>
+				<th>Fruit</th>
+				<th>Jozani</th>
+				<th>05</th>
+			</tr>
+		</tbody>
+	</table>
+</div>
